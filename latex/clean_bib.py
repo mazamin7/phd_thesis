b7e36c2a1f32e49d8f6771cbb88fda84aae6c720
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2026-09-03 11:11:42 +0200

    Implemented Ilario suggestions of 03/09

---
+++ latex/clean_bib.py
@@ -0,0 +1,16 @@
+﻿with open('Thesis_bibliography.bib', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace('\u2010', '-')
+content = content.replace('\u2011', '-')
+content = content.replace('\u2012', '-')
+content = content.replace('\u2013', '--')
+content = content.replace('\u2014', '---')
+content = content.replace('\u2015', '---')
+content = content.replace('\u2018', "'")
+content = content.replace('\u2019', "'")
+content = content.replace('\u201c', '"')
+content = content.replace('\u201d', '"')
+
+with open('Thesis_bibliography.bib', 'w', encoding='utf-8') as f:
+    f.write(content)
---
+++ latex/clean_tex.py
@@ -0,0 +1,18 @@
+﻿import glob
+for file in glob.glob('*.tex'):
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    content = content.replace('\u2010', '-')
+    content = content.replace('\u2011', '-')
+    content = content.replace('\u2012', '-')
+    content = content.replace('\u2013', '--')
+    content = content.replace('\u2014', '---')
+    content = content.replace('\u2015', '---')
+    content = content.replace('\u2018', "'")
+    content = content.replace('\u2019', "'")
+    content = content.replace('\u201c', '"')
+    content = content.replace('\u201d', '"')
+
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(content)
